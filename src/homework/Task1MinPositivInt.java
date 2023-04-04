9501fec1ca0b5f67bfd88df92fbddd7dcf40b6fa
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-05 00:21:37 +0200

    Initial commit - add main

--- src/homework/Task1MinPositivInt.java
+++ src/homework/Task1MinPositivInt.java
@@ -1,11 +1,36 @@
 package homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // Найти минимальное чётное число в последовательности положительных чисел или вывести -1,
 // если такого числа не существует.
 // Оценить временную и пространственную сложность алгоритма.
 public class Task1MinPositivInt {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    System.out.print("Введите количество чисел: ");
+    int n = Integer.parseInt(br.readLine());
+
+    System.out.println("Введите " + n + " положительных чисел, каждое с новой строки:");
+    int result = -1;
+    int min = Integer.parseInt(br.readLine());
+    for (int i = 1; i < n; ++i) {
+      int x = Integer.parseInt(br.readLine());
+      if (x % 2 == 0) {
+        if (x < min) {
+          min = x;
+          result = i;
+        }
+      }
+    }
+    if (result < 0) {
+      System.out.println("Результат: " + result);
+    } else {
+      System.out.println("Минимальное чётное число: " + min);
+    }
   }
 }
